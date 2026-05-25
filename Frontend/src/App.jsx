10b7384edfa23c4f9809b95a94e5818858662b71
Akash A <akashaakasha290@gmail.com>
2026-05-25 16:37:15 +0530

    Frontend Dev

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,120 +1,88 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [file, setfile] = useState(null)
+  const [question, setquestion] = useState("")
+  const [answer, setanswer] = useState("")
+  const [loading, setloading] = useState(false)
+
+  const handleUpload = async () => {
+    if (!file) {
+      alert("please Select a PDF file to Upload")
+      return
+    }
+    const formdata = new FormData()
+    formdata.append("file", file)
+    setloading(true)
+    try {
+      await axios.post("backendurl", formdata)
+      alert("File Uploaded Successfully")
+    }
+    catch (error) {
+      console.log(error)
+      alert("Error uploading file")
+    }
+    setloading(false)
+
+  }
+
+  const handleQuestion = async () => {
+    if (!question) {
+      alert("Please enter a question")
+      return
+    }
+    setloading(true)
+    try {
+      const response = await axios.post("backendurls", { question })
+      setanswer(response.data.answer)
+    }
+    catch (error) {
+
+      console.log(error)
+      alert("Error in getting the answer")
+    }
+    setloading(false)
+
+  }
+
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+      <div className="container">
+        <h1 className="title">PDF QUESTION ANSWERING SYSTEM</h1>
+        <div className="file-input">
+          <input type="file" accept="application/pdf" onChange={(e) => setfile(e.target.files[0])} />
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+        <button onClick={handleUpload}>Upload PDF</button>
+        <br /><br />
 
-      <div className="ticks"></div>
+      
+      <div className="question-section">
+        <input type="text" placeholder="Ask a question..." value={question} onChange={(e) => setquestion(e.target.value)} />
+      <button onClick={handleQuestion}>Ask Question</button>
+      </div>
+      
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      {
+        loading ? <p>Loading...</p> : null
+      }
+
+      {
+        answer && <div className="answer-section">
+          <h2>Answer:</h2>
+          <p>{answer}</p>
+          <div className="button-section">
+            The Answer will be based on the content of the uploaded PDF file.
+          </div>
+        </div>
+
+      }
+</div>
+
+
     </>
   )
 }
